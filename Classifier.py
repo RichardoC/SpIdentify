790668b3bce54f1ac3ff80222170f9eda398a415
Richard F. Tweed <richard.tweed.13@ucl.ac.uk>
2016-11-26 17:58:41 +0000

    Almost working single layer perceptron

--- Classifier.py
+++ Classifier.py
@@ -1,3 +1,44 @@
-import tensorflow as tf
+import  sklearn as sk
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
 
-print "fjfhfh"
+
+spiders = glob.glob('./Spider/*')
+non_spiders = glob.glob('./NonSpider/*')
+
+SPimg = []
+for sp in spiders:
+    img = Image.open(sp)
+    resized = scipy. misc.imresize(img,[100,100])
+    SPimg.append(resized)
+
+
+NSPimg = []
+
+for sp in non_spiders:
+    img = Image.open(sp)
+    resized = scipy.misc.imresize(img,[100,100])
+    NSPimg.append(resized)
+
+testImgs = SPimg[0:int(0.75*len(SPimg))]
+testLabs = np.ones(int(0.75*len(SPimg)))
+
+testImgs[len(testImgs):(len(testImgs)+(int)(0.75*len(NSPimg)))] = SPimg[0:(int)(0.75*len(NSPimg))]
+testLabs[len(testLabs):(len(testLabs)+(int)(0.75*len(NSPimg)))] = - np.ones(int(0.75*len(NSPimg)))
+
+
+
+#Now for a single layer perceptron with 3 nodes in the hidden layer
+SLPClass = sk.MLPClassifier(hidden_layer_sizes=(3,))
+SLPClass.fit(testImgs,testLabs)
+
+testSP = SPimg[(int)(0.75*len(SPimg))+1:-1]
+testSLPSPScore = SLPClass.score(testSP,np.ones(len(testSP)))
+print "Testing the SLP on spiders " , testSLPSPScore
+
+testNSP = NSPimg[(int)(0.75*len(NSPimg))+1:-1]
+testNSLPSPScore = SLPClass.score(testNSP,np.ones(len(testNSP)))
+print "Testing the SLP on non-spiders " , testNSLPSPScore
