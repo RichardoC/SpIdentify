b2f1bb211a570966ce3d085fd4a80ac386071b88
Richard F. Tweed <richard.tweed.13@ucl.ac.uk>
2016-11-26 18:18:53 +0000

    Working Classifier (albeit bad) with no test cases

--- Classifier.py
+++ Classifier.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 from PIL import Image
-
+from sklearn.neural_network import MLPClassifier
 
 spiders = glob.glob('./Spider/*')
 non_spiders = glob.glob('./NonSpider/*')
@@ -15,30 +15,38 @@ for sp in spiders:
     resized = scipy. misc.imresize(img,[100,100])
     SPimg.append(resized)
 
-
 NSPimg = []
-
 for sp in non_spiders:
     img = Image.open(sp)
     resized = scipy.misc.imresize(img,[100,100])
     NSPimg.append(resized)
 
+testSize = int(0.75*len(SPimg)) + int(0.75*len(NSPimg))
+testLabs = np.zeros(testSize)
+
 testImgs = SPimg[0:int(0.75*len(SPimg))]
-testLabs = np.ones(int(0.75*len(SPimg)))
+for i in range(0,int(0.75*len(NSPimg))):
+    testLabs[i] = 1
+# testLabs = np.ones(int(0.75*len(SPimg)))
 
 testImgs[len(testImgs):(len(testImgs)+(int)(0.75*len(NSPimg)))] = SPimg[0:(int)(0.75*len(NSPimg))]
-testLabs[len(testLabs):(len(testLabs)+(int)(0.75*len(NSPimg)))] = - np.ones(int(0.75*len(NSPimg)))
+toAloc = -1*np.ones(int(0.75*len(NSPimg)))
 
+for i in range(int(0.75*len(NSPimg)),(len(testImgs)-2)):
+    testLabs[i] = -1
 
+testImgs = np.array(testImgs)
+TwoDim_dataset = testImgs.reshape(testSize,-1)
 
 #Now for a single layer perceptron with 3 nodes in the hidden layer
-SLPClass = sk.MLPClassifier(hidden_layer_sizes=(3,))
-SLPClass.fit(testImgs,testLabs)
+SLPClass = MLPClassifier(hidden_layer_sizes=(3,))
+SLPClass.fit(TwoDim_dataset,testLabs)
 
-testSP = SPimg[(int)(0.75*len(SPimg))+1:-1]
-testSLPSPScore = SLPClass.score(testSP,np.ones(len(testSP)))
-print "Testing the SLP on spiders " , testSLPSPScore
-
-testNSP = NSPimg[(int)(0.75*len(NSPimg))+1:-1]
-testNSLPSPScore = SLPClass.score(testNSP,np.ones(len(testNSP)))
-print "Testing the SLP on non-spiders " , testNSLPSPScore
+print SLPClass.loss_
+# testSP = SPimg[(int)(0.75*len(SPimg))+1:-1]
+# testSLPSPScore = SLPClass.score(testSP,np.ones(len(testSP)))
+# print "Testing the SLP on spiders " , testSLPSPScore
+#
+# testNSP = NSPimg[(int)(0.75*len(NSPimg))+1:-1]
+# testNSLPSPScore = SLPClass.score(testNSP,np.ones(len(testNSP)))
+# print "Testing the SLP on non-spiders " , testNSLPSPScore
