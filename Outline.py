510085afc8795bb017ccc499194c8c74cb62dd59
Jonathan Mounty <jonathan.mounty@kcl.ac.uk>
2016-11-27 01:00:51 +0000

    Improved dataset

--- Outline.py
+++ Outline.py
@@ -1,16 +1,17 @@
 from PIL import Image
 import os
 
-folderName = "Spider"
+folderNames = {"Spider", "NonSpider", "BigSpiders", "topdown"}
 thresholdLow = 150
 thresholdHigh = 255
 
-if (not os.path.isdir('Edited' + folderName)) :
-	os.makedirs('Edited' + folderName)
+for folderName in folderNames :
+	if (not os.path.isdir('Edited' + folderName)) :
+		os.makedirs('Edited' + folderName)
 
-for fileName in os.listdir( folderName) :
-    image = Image.open(folderName + "/"+fileName)
-    mask=image.convert("L")
-    
-    mask = mask.point(lambda i: i < thresholdLow and thresholdHigh)
-    mask.save("Edited" + folderName +"/"+fileName)
+	for fileName in os.listdir( folderName) :
+		image = Image.open(folderName + "/"+fileName)
+		mask=image.convert("L")
+
+		mask = mask.point(lambda i: i < thresholdLow and thresholdHigh)
+		mask.save("Edited" + folderName +"/"+fileName)
