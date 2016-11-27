d43d18ebe975b4b49264d762ae04142e16146e69
Richard F. Tweed <richard.tweed.13@ucl.ac.uk>
2016-11-27 10:26:45 +0000

    Trying to run test on localhost

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import cgi
+
+from bs4 import BeautifulSoup
+
 import IsSpider
 import urllib
 
 testfile = urllib.URLopener()
-testfile.retrieve("http://randomsite.com/file.gz", "file.gz")
+
 
 url = 'index.html'
 page = open(url)
-soup = BeautifulSoup(page.read(), 'lxml')
+soup = BeautifulSoup(page.read(), 'html.parser')
 tag = soup.p
 
 
@@ -20,21 +23,23 @@ with open("index.html", "wb") as file:
     file.write(html)
 
 # dodgy bit (no local host):
+form = cgi.FieldStorage()
 while True:
-	form = cgi.FieldStorage()
-	link =  form.getvalue('text')
-	if link: break
+    link =  form.getvalue('text')
+    if link:
+        print "link found"
+        break
 
-tempImg = urllib.request.urlretrieve(link,'temp.jpg')
-
+# tempImg = urllib.request.urlretrieve(link,'temp.jpg')
+tempImg = testfile.retrieve(link,'temp.jpg')
 output = IsSpider.isSpider('./temp.jpg') ###############################################################<<<-------- result of program
 
 if output==1:
-	text = 'Beware! There is a spider in the picture.'
+    text = 'Beware! There is a spider in the picture.'
 elif output ==0:
     text = "No need to worry! There isn't a spider in the picture."
 else:
-	test = "Issue with the image/url"
+    test = "Issue with the image/url"
 
 tag.string = text
 print(link)
