47bdb40fec61a16123a256fa87a6ddb6c866cfa5
Richard F. Tweed <richard.tweed.13@ucl.ac.uk>
2016-11-28 11:53:34 +0000

    IsSpider Realised we weren't taking the outlines on the new images, possibly lowering real world performance

--- IsSpider.py
+++ IsSpider.py
@@ -15,19 +15,23 @@ import pickle
 def isSpider(location):
     fileLoc = location #= "./EditedOutlineImages/NonSpider/1.jpg"
 
+    thresholdLow = 150
+    thresholdHigh = 255
+
     with open('SLPClass.pkl', 'rb') as input:
         SLPClass = pickle.load(input)
 
     img = Image.open(fileLoc).convert('L')
+    OutLineImg = img.point(lambda i: i < thresholdLow and thresholdHigh)
 
     px = 20
     py = 10
 
     SPimg = []
 
-    width, height = img.size
+    width, height = OutLineImg.size
     if ((width >= px) and (height >= py)):
-        resized = scipy.misc.imresize(img, [px, py])
+        resized = scipy.misc.imresize(OutLineImg, [px, py])
         SPimg.append(resized)
     else:
         return 100
@@ -41,3 +45,5 @@ def isSpider(location):
 print "1 = spider, 0 = non spider"
 print "testing on a non spider image",isSpider("./EditedOutlineImages/NonSpider/1.jpg")
 print "testing on a spider image ",isSpider("./EditedOutlineImages/Spider/_86087350_028938243-1.jpg")
+
+print "spiderman", isSpider("/Users/richardf/Downloads/spiderman.jpg")
